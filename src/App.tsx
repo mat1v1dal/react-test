37a3ab97ead2ecfec05af486984555f3336d7315
mat1v1dal <2302403@ucc.edu.ar>
2024-05-17 13:02:07 -0300

    componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import Titulo from "./Titulo";
-
+import Card, { CardBody } from "./components/Card";
+import List from "./components/List";
 function App() {
-  return <Titulo />;
+  const list: string[] = ["uno", "dos", "tres"];
+  const handleSelect = (elemento: string) => {
+    console.log("imprimiendo", elemento);
+  };
+  const contenido = list.length !== 0 && (
+    <List data={list} onSelect={handleSelect} />
+  );
+
+  return (
+    <Card>
+      <CardBody title="Titulo" text="hola mundo" />
+      {contenido}
+    </Card>
+  );
 }
 
 export default App;
